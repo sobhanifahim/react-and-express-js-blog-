fe57ff449590be54b6047697f44298d2e101803d
sobhanifahim <fahim.sobhnai12@gmail.com>
2023-10-07 00:02:54 +0600

    final api commit

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors({
   origin:["http://localhost:3000"],
-  methods: ["POST","GET"],
+  methods: ["POST","GET","DELETE"],
   credentials:true
 }));
 app.use(cookie())
@@ -29,7 +29,7 @@ app.use(session({
   secret: 'your-secret-key',
   resave: false,
   saveUninitialized: false,
-  cookie: { maxAge: 3600000 }
+  cookie: { maxAge: 360000000 }
 }));
 const db = mysql.createConnection({
   host: 'localhost',
@@ -130,17 +130,17 @@ app.get('/userprofile', (req, res) => {
   const user = req.session.user;
   const name=user.username
   if (!req.session || !req.session.user) {
-    // User is not authenticated, handle this case (e.g., redirect to login)
+
     return res.status(401).json({ message: 'Unauthorized' });
   }
   
   if (!user) {
-    // User is not authenticated, redirect to the login page
+
     res.redirect('/');
     return;
   }
 
-  // Fetch the user's profile image from the database
+
   db.query('SELECT uimg FROM userprofile WHERE uid = ?', [user.id], (error, results) => {
     if (error) {
       console.error('Error fetching profile image:', error);
@@ -150,26 +150,15 @@ app.get('/userprofile', (req, res) => {
     let userImageDataUri = null;
 
     if (results.length > 0 && results[0].uimg !== null) {
-      // Assuming results[0].uimg contains the image data (make sure it's a valid URL or base64 data)
+
       const userImageBuffer = results[0].uimg;
       const userImageBase64 = userImageBuffer.toString('base64');
 
-      // Create a data URI
+
       userImageDataUri = `data:image/jpeg;base64,${userImageBase64}`;
     }
      res.send({name,userImageDataUri})
 
-    // // Read the HTML file
-    // const fs = require('fs');
-    // const profileHtml = fs.readFileSync(path.join(__dirname, 'public', 'profile.html'), 'utf8');
-
-    // // Replace the placeholders in the HTML file
-    // const updatedHtml = profileHtml
-    //   .replace('${user.username}', user.username)
-    //   .replace('${user.image}', userImageDataUri);
-
-    // // Send the updated HTML content as the response
-    // res.send(updatedHtml);
   });
 });
 
@@ -205,55 +194,35 @@ app.post('/userprofile',upload.single('image'),(req, res) => {
 });
 
  
-// Serve the home.html file for the /home route
+
 app.get('/home', (req, res) => {
-  // Retrieve data from the 'blog' table
+  
   db.query('SELECT * FROM blog', (error, results) => {
     if (error) {
       console.error('Error fetching blog data', error);
       return res.status(500).json({ message: 'Error fetching blog data.' });
     }
 
-    // Assuming 'results' contains an array of blog posts
+    
     const blogPosts = results;
     res.send(blogPosts)
-    // Read the 'home.html' file
-  //   fs.readFile(path.join(__dirname, 'public', 'home.html'), 'utf8', (readError, fileContents) => {
-  //     if (readError) {
-  //       console.error('Error reading HTML file', readError);
-  //       return res.status(500).json({ message: 'Error reading HTML file.' });
-  //     }
-
-  //     // Replace placeholders with dynamic data
-  //     const updatedHtml = fileContents.replace('<!-- Dynamic content goes here -->', () => {
-  //       let dynamicContent = '';
-
-  //       blogPosts.forEach((post) => {
-  //         dynamicContent += `
-  //         <a href="/readblog/${post.author}/${post.bid}">
-  //         <div class="blog-post" style="background-color: gainsboro">
-  //             <h3>${post.title}</h3>
-  //             <p>Author: ${post.author}</p>
-  //           </div></a>
-  //         `;
-  //       });
-
-  //       return dynamicContent;
-  //     });
-
-  //     // Send the updated HTML content as the response
-  //     res.send(updatedHtml);
-  //   });
-   });
+    
+  });
 });
 
 
-app.post('/createblog', (req, res) => {
+app.post('/createblog',upload.single('image'),(req, res) => {
   const bdata=req.body;
+  const image=req.file;
   const user = req.session.user;
   // console.log(user)
   // console.log(bdata)
-  db.query('INSERT INTO blog (uid, title, content,author) VALUES (?, ?, ?,?)', [user.id,bdata.title,bdata.content,user.username], (error, results) => {
+  if (!image) {
+    return res.status(400).json({ message: 'Image data is missing.' });
+  }
+  const fs = require('fs');
+  const imageData = fs.readFileSync(image.path);
+  db.query('INSERT INTO blog (uid,bimg, title, content,author) VALUES (?, ?, ?,?,?)', [user.id,imageData,bdata.title,bdata.content,user.username], (error, results) => {
     if (error) {
       console.error('Error creating your blog', error);
       return res.status(500).json({ message: 'Error creating blog.' });
@@ -271,107 +240,85 @@ app.post('/createblog', (req, res) => {
 
 app.get('/readblog/:author/:blogid', (req, res) => {
   const blogId = req.params.blogid;
-
-  // Fetch the blog post data from the database using the 'blogId'
+  
+  
   db.query('SELECT * FROM blog WHERE bid = ?', [blogId], (error, results) => {
     if (error) {
       console.error('Error fetching blog post data:', error);
       return res.status(500).json({ message: 'Error fetching blog post data.' });
     }
 
-    // Check if the blog post with the specified 'blogId' exists
+  
     if (results.length === 0) {
       return res.status(404).send('Blog post not found');
     }
 
-    // Assuming 'results[0]' contains the blog post data
+    
     const blogPost = results[0];
 
-    // Fetch comments related to the blog post from the comments table
+    let userImageDataUri = null;
+
+    if (results.length > 0 && results[0].bimg !== null) {
+
+      const userImageBuffer = results[0].bimg;
+      const userImageBase64 = userImageBuffer.toString('base64');
+
+
+      userImageDataUri = `data:image/jpeg;base64,${userImageBase64}`;
+    }
+
+    
     db.query('SELECT * FROM comments WHERE bid = ?', [blogId], (commentError, commentResults) => {
       if (commentError) {
         console.error('Error fetching comments:', commentError);
         return res.status(500).json({ message: 'Error fetching comments.' });
       }
 
-      // Assuming 'commentResults' contains an array of comments
-      const comments = commentResults;
-
-      const fs = require('fs');
-      const readBlogHtml = fs.readFileSync(path.join(__dirname, 'public', 'readblog.html'), 'utf8');
-
-      const commentedHtml = (() => {
-        let dynamicContent = '';
-
-        comments.forEach((post) => {
-          dynamicContent += `
-          <ul>
-          <li class="blog-post" style="background-color: gainsboro">
-             <div>
-             <i>${post.commenter}</i><br>
-              ${post.comments}
-              </div>
-            </li>
-          </ul>
-          `;
-        });
-
-        return dynamicContent;
-      })();
-
       
-      const updatedHtml = readBlogHtml
-        .replace('{blogtitle}', blogPost.title)
-        .replace('{author}', blogPost.author)
-        .replace('{date}', blogPost.created_at)
-        .replace('{blogcontetnt}', blogPost.content)
-        .replace('{{author}}', blogPost.author)
-        .replace('{{blogid}}', blogPost.bid)
-        .replace('{{auth}}', blogPost.author)
-        .replace('{{bid}}', blogPost.bid)
-        .replace('{{comments}}', commentedHtml);
-
-      // Pass the comments data to your HTML template or handle it as needed
-      // You can include the comments in your HTML, perhaps inside a loop or another HTML element.
-
-      res.send(updatedHtml);
+      const comments = commentResults;
+      res.send(comments)
+      
     });
   });
 });
 
+
 app.post('/readblog/:author/:blogid', (req, res) => {
   const blogId = req.params.blogid;
-  const formName = req.body.formName; // Add a hidden input field with the form's name
   const user = req.session.user;
-  if (formName === 'delete') {
-      // Handle blog post deletion
-      db.query('DELETE FROM blog WHERE bid = ?', [blogId], (error, results) => {
-          if (error) {
-              console.error('Error deleting blog post:', error);
-              return res.status(500).json({ message: 'Error deleting blog post.' });
-          }
 
-          // Redirect to the '/home' page after deletion 
-          res.redirect('/home');
-      });
-  } else if (formName === 'comments') {
-      // Handle posting comments
       const comment = req.body.comment;
-      // Insert the comment into the database, associate it with the blog post (using 'blogId')
-      // Redirect back to the same blog post or handle as needed
       db.query('INSERT INTO comments (uid, bid,commenter, comments) VALUES (?, ?, ?,?)', [user.id,blogId,user.username,comment], (error, results) => {
         if (error) {
             console.error('Error deleting blog post:', error);
             return res.status(500).json({ message: 'Error deleting blog post.' });
         }
         res.json({ message: 'commented successfully' })
-        // Redirect to the '/home' page after deletion
         
     });
-
-  }
+});
+app.delete('/readblog/:author/:blogid', (req, res) => {
+  const blogId = req.params.blogid;
+  db.query('DELETE FROM blog WHERE bid = ?', [blogId], (error, results) => {
+    if (error) {
+        console.error('Error deleting blog post:', error);
+        return res.status(500).json({ message: 'Error deleting blog post.' });
+    }
+    res.json({ message: 'Deleted the blog' })
+}); 
 });
 
+app.get('/logout', (req, res) => {
+  
+  req.session.destroy((err) => {
+    if (err) {
+      console.error('Error destroying session:', err);
+    } else {
+      
+      res.json({ message: 'User Logged out' })
+    }
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
